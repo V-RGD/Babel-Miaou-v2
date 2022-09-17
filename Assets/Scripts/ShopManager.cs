dd0224acdc7224c752629fbf6d79c75d89cd914c
V_RGD <v.regard@rubika-edu.com>
2022-09-18 01:24:46 +0200

    IA / Noeils / Dégats et Barres de vie
    hopefully it won't break soon

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,6 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ShopManager : MonoBehaviour
 {
@@ -12,14 +13,18 @@ public class ShopManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < 3; i++)
+        {
+            spawnAreas.Add(transform.GetChild(i).transform);
+        }
+        
         //choses 3 random items from a list of gameobjects
         //spawns them in 3 different transforms
         for (int i = 0; i < 3; i++)
         {
-            Instantiate(items[Random.Range(0, items.Length)], spawnAreas[i]);
+            Instantiate(items[Random.Range(0, items.Length)], spawnAreas[i].position + Vector3.up * 6, quaternion.identity, spawnAreas[i]);
         }
         //if the player enters the object's area, has the right amount of cash, and presses the right button, items dissapear,
         //gives proper effect to the player, and removes money from player
-        
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,28 +1,23 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public GameManager gameManager;
-    public TMP_Text HeightUI;
-    public TMP_Text CurrentLevelUI;
+    public TMP_Text MoneyUI;
+    public TMP_Text HeartsUI;
+
     
     private void Awake()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
-        HeightUI.text = gameManager.height + "m";
-        CurrentLevelUI.text = "Level " + gameManager.currentLevel;
+        MoneyUI.text = gameManager.money.ToString();
+        HeartsUI.text = gameManager.health.ToString();
     }
+
+    
 }

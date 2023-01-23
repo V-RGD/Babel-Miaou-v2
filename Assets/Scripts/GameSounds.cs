bf9b23255635e586f46bdb997c97d7969f027a03
V_RGD <v.regard@rubika-edu.com>
2023-01-23 13:34:14 +0100

    sons pour a peu près tout dans le jeu

---
+++ Assets/Scripts/GameSounds.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSounds : MonoBehaviour
+{
+    [Header("Enemies")]
+    public AudioClip[] enemyInvocation;
+    public AudioClip[] enemyDeath;
+    public AudioClip[] wandererClaw;
+    public AudioClip[] bullSteps;
+    public AudioClip[] bullDash;
+    public AudioClip[] bullKnock;
+    public AudioClip[] bullHurt;
+    public AudioClip[] shooterSteps;
+    public AudioClip[] shooterShoot;
+    public AudioClip[] shooterProjectile;
+    public AudioClip[] shooterRecharge;
+    public AudioClip[] marksmanCharge;
+    public AudioClip[] marksmanShoot;
+    public AudioClip[] bossClaw;
+    public AudioClip[] bossInvocation;
+    public AudioClip[] bossRock;
+    public AudioClip[] bossLaser;
+    public AudioClip[] bossLaugh;
+    public AudioClip[] bossHurt;
+    public AudioClip[] bossAttack;
+    [Header("Player")]
+    public AudioClip[] playerHeal;
+    public AudioClip[] playerSteps;
+    public AudioClip[] playerAttacks;
+    public AudioClip[] playerHeartbeat;
+    public AudioClip[] playerHurt;
+    [Header("UI")]
+    public AudioClip[] itemAcquired;
+    public AudioClip[] itemPurchased;
+    public AudioClip[] uiClick;
+    public AudioClip[] menuChange;
+    public AudioClip[] coinEarned;
+    [Header("Environment")]
+    public AudioClip[] braseroFireplace;
+}
